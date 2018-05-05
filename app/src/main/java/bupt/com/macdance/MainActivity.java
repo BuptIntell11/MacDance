78e13927d86772c9211872bf6c4456850e58fbee
Y2015212055 <nicoleynh960111@163.com>
2018-05-06 01:04:13 +0800

    界面弄了一些

---
+++ app/src/main/java/bupt/com/macdance/MainActivity.java
@@ -0,0 +1,39 @@
+package bupt.com.macdance;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class MainActivity extends AppCompatActivity {
+
+    private String[] names = new String[]{"鸿雁", "鸿雁", "鸿雁"};
+    private String[] says = new String[]{"鸿雁，鸿雁", "大神好厉害~", "鸿雁~"};
+    private int[] imgIds = new int[]{R.drawable.photo,R.drawable.photo,R.drawable.photo};
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        List<Map<String, Object>> listitem = new ArrayList<Map<String, Object>>();
+        for (int i = 0; i < names.length; i++) {
+            Map<String, Object> showitem = new HashMap<String, Object>();
+            showitem.put("touxiang", imgIds[i]);
+            showitem.put("name", names[i]);
+            showitem.put("says", says[i]);
+            listitem.add(showitem);
+        }
+
+        //创建一个simpleAdapter
+        SimpleAdapter myAdapter = new SimpleAdapter(getApplicationContext(), listitem, R.layout.list_item, new String[]{"touxiang", "name", "says"}, new int[]{R.id.imgtou, R.id.name, R.id.says});
+        ListView listView = (ListView) findViewById(R.id.list_test);
+        listView.setAdapter(myAdapter);
+    }
+}
